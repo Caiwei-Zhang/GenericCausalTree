e27b5ee288d44562a9f5cf6c32a7e13b95bc0e94
ZHANG CAIWEI <44227292+Caiwei-Zhang@users.noreply.github.com>
2022-05-20 09:53:41 +0800

    Update eval.honest.tree.R

--- Functions/eval.honest.tree.R
+++ Functions/eval.honest.tree.R
@@ -142,8 +142,6 @@ eval.honest.tree <- function(tree, data.test, eff.modifier, true.trt.eff, trueGr
   # # mse will be NA if there is only 1 observation in the terminal node
   # = mean((pred.tree - true.trt.eff)^2, na.rm = T)
   
-  # Measure 8: widthCI
-  widthCI <- mean(tree$frame$widthCI[tree$frame$var == "<leaf>"])
   
   # Measure 9: coverage of confidence interval
   if (is.null(eff.modifier)) { # homo
@@ -152,11 +150,25 @@ eval.honest.tree <- function(tree, data.test, eff.modifier, true.trt.eff, trueGr
     est.trt.eff.leaf <- tree$frame$yval[1]
     
     if (corr.tree == 1) {
-      bl <- (abs(est.trt.eff.leaf - true.trt.eff.leaf) <= qnorm(0.975) * sqrt(tree$frame$variance[1]))
+      #bl <- (abs(est.trt.eff.leaf - true.trt.eff.leaf) <= qnorm(0.975) * sqrt(tree$frame$variance[1]))
+      bl <- (tree$frame$ci.lower[1] <= true.trt.eff.leaf) & (tree$frame$ci.upper[1] >= true.trt.eff.leaf)
       cvr <- ifelse(bl == 1, 1, 0)
+      
+      # Measure 8: width of confidence interval, remove in Output() if corr.tree == 0
+      widthCI <- tree$frame$widthCI[1]
+      
     } else {
+      
       cvr <- 0
+      widthCI <- NA
+      
     }
+    
+    
+    # Measure 10: average coverage
+    test.frame <- TrueATE(data.test = data.test, tree = tree, true.trt.eff = true.trt.eff)
+    avg_cov <- sum((test.frame$ci.lower < test.frame$true.avg.trt.eff) & (test.frame$true.avg.trt.eff < test.frame$ci.upper))/nrow(tree$frame)
+    
 
     results <- data.frame(fir.splt.ind   = fir.splt.ind, 
                           abs.err.sp     = abs.err.sp,
@@ -164,37 +176,64 @@ eval.honest.tree <- function(tree, data.test, eff.modifier, true.trt.eff, trueGr
                           num.leaf       = num.leaf,
                           num.noise.splt = num.noise.splt, 
                           pps  = pps,  mse  = mse, 
-                          cvr = cvr, widthCI = widthCI) 
+                          cvr = cvr, 
+                          cov_ratio = avg_cov,
+                          widthCI = widthCI) 
   } else {  # hetero 
     
     if (corr.tree == 1) {
+      
       est.trt.eff.leaf <- tree$frame$yval[tree$frame$var == "<leaf>"]
       
-      if (abs(est.trt.eff.leaf[1] - 2) < abs(est.trt.eff.leaf[1] - 5)){
+      if (abs(est.trt.eff.leaf[1] - 2) < abs(est.trt.eff.leaf[1] - 5)){ # left is 2, right is 5
+        
         true.trt.eff.leaf <- c(2, 5)
+        
       } else {
+        
         true.trt.eff.leaf <- c(5, 2)
+        
       }
        
-      bl <- abs(est.trt.eff.leaf - true.trt.eff.leaf) <= qnorm(0.975) * sqrt(tree$frame$variance[tree$frame$var == "<leaf>"])
-     
+      # bl <- abs(est.trt.eff.leaf - true.trt.eff.leaf) <= qnorm(0.975) * sqrt(tree$frame$variance[tree$frame$var == "<leaf>"])
+      bl <- (tree$frame$ci.lower[tree$frame$var == "<leaf>"] <= true.trt.eff.leaf) & (tree$frame$ci.upper[tree$frame$var == "<leaf>"] >= true.trt.eff.leaf)
+      
       # simultaneous confidence inference: a larger quantile 
       sim_bl <- abs(est.trt.eff.leaf - true.trt.eff.leaf) <= qnorm(1-(1-sqrt(0.95))/2)*sqrt(tree$frame$variance[tree$frame$var == "<leaf>"]) 
       sim_cvr <- ifelse(sum(sim_bl) == 2, 1, 0)
       
       if(true.trt.eff.leaf[1] == 2) {
+        
         cvr1 <- ifelse(bl[1] == 1, 1, 0)
         cvr2 <- ifelse(bl[2] == 1, 1, 0)
+        
+        widthCI1 <- tree$frame$widthCI[tree$frame$var == "<leaf>"][1]
+        widthCI2 <- tree$frame$widthCI[tree$frame$var == "<leaf>"][2]
+        
       } else {
+        
         cvr1 <- ifelse(bl[2] == 1, 1, 0)
         cvr2 <- ifelse(bl[1] == 1, 1, 0)
+        
+        widthCI1 <- tree$frame$widthCI[tree$frame$var == "<leaf>"][2]
+        widthCI2 <- tree$frame$widthCI[tree$frame$var == "<leaf>"][1]
+        
       }
       
     } else {
+      
       cvr1 <- cvr2 <- 0
       sim_cvr <- 0
+      
+      widthCI1 <- widthCI2 <- mean(tree$frame$widthCI[tree$frame$var == "<leaf>"])
+      
     }
-
+    
+    
+    # Measure 10: avgerage coverage
+    test.frame <- TrueATE(data.test = data.test, tree = tree, true.trt.eff = true.trt.eff)
+    avg_cov <- sum((test.frame$ci.lower < test.frame$true.avg.trt.eff) & (test.frame$true.avg.trt.eff < test.frame$ci.upper))/nrow(tree$frame)
+    
     results <- data.frame(fir.splt.ind   = fir.splt.ind, 
                       abs.err.sp = abs.err.sp,
                       corr.tree = corr.tree, 
@@ -202,8 +241,9 @@ eval.honest.tree <- function(tree, data.test, eff.modifier, true.trt.eff, trueGr
                       num.noise.splt = num.noise.splt,
                       pps  = pps,  mse  = mse, 
                       cvr1 = cvr1, cvr2 = cvr2, 
-                      sim_cvr = sim_cvr,
-                      widthCI = widthCI)
+                      sim_cvr = sim_cvr, 
+                      cov_ratio = avg_cov,
+                      widthCI1 = widthCI1, widthCI2 = widthCI2)
   }
 }
   
